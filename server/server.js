39ac22aec7fe8028bfe1f7f96cd62a3cf6cf0080
Jordan Ferrario <108853449+JordanFerrario@users.noreply.github.com>
2024-04-11 18:52:15 -0700

    added tailwindcss framework to repo

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-// import express and ApolloServer, including express middleware for Apollo 
-const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
-const path = require('path');
+// import express and ApolloServer, including express middleware for Apollo
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const path = require("path");
 // import authMiddleware from auth.js
-const { authMiddleware } = require('./utils/auth');
-// import typeDefs & resolvers for server fetches from schemas, and the db connection from config 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { authMiddleware } = require("./utils/auth");
+// import typeDefs & resolvers for server fetches from schemas, and the db connection from config
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -23,20 +23,23 @@ const startApolloServer = async () => {
 
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
-// use GraphQL API for data management
-  app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
-  }));
+  // use GraphQL API for data management
+  app.use(
+    "/graphql",
+    expressMiddleware(server, {
+      context: authMiddleware,
+    })
+  );
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
     });
   }
 
-  db.once('open', () => {
+  db.once("open", () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
@@ -45,4 +48,4 @@ const startApolloServer = async () => {
 };
 
 // Call the async function to start the server
-  startApolloServer();
+startApolloServer();
