63414fb27f006b35de50a0701dda5530710c461f
Jeffreydne <Jeffreydne@gmail.com>
2024-04-12 06:56:32 -0700

    add border to nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,10 +6,11 @@ const AppNavbar = () => {
 
    return (
     <>
-        <div className='flex items-center justify-between bg-gray-700 p-4 h-full'>
+        <div className='flex items-center justify-between bg-gray-700 p-4 h-full border-b-2'>
             <h2 className='text-[24-px font-bold text-blue-400'>Twighlight Emporium Tracker</h2>
             <h2 className='text-[24-px font-bold text-blue-500 hover:border-[2px] border-blue-500 rounded-md p-1 text-[15px] cursor-pointer'>LOGOUT</h2>
         </div>
+        
     </>
    ); 
 };
