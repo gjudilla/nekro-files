e3017eee9f9ecaa4b2600a36bf3c5d1c67ebb3aa
Jeffreydne <Jeffreydne@gmail.com>
2024-04-12 06:43:17 -0700

    add Navbar.jsx component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
 import './main.css';
+import AppNavbar from './components/Navbar';
 
 function App() {
     return (
-        <Dashboard phase="action" /> // or "agenda" for the agenda phase
+        <>
+            <AppNavbar />
+            <Dashboard phase="action" /> // or "agenda" for the agenda phase
+        </>
     )
 }
 
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+
+const AppNavbar = () => {
+    const [loggedIn, setLoggedIn] = useState(false);
+
+   return (
+    <>
+        <div className='flex items-center justify-between bg-gray-700 p-4 h-full'>
+            <h2 className='text-[24-px font-bold text-blue-400'>Twighlight Emporium Tracker</h2>
+            <h2 className='text-[24-px font-bold text-blue-500 hover:border-[2px] border-blue-500 rounded-md p-1 text-[15px] cursor-pointer'>LOGOUT</h2>
+        </div>
+    </>
+   ); 
+};
+export default AppNavbar
