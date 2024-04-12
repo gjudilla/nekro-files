dbac3ff2a4794d31f799f54878fc9941c9aee043
Jeffreydne <Jeffreydne@gmail.com>
2024-04-12 13:14:28 -0700

    set up initial login vs dashboard page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import { Link } from 'react-router-dom';
 
 const AppNavbar = () => {
     const [loggedIn, setLoggedIn] = useState(false);
@@ -8,7 +8,20 @@ const AppNavbar = () => {
     <>
         <div className='flex items-center justify-between bg-gray-700 p-4 h-full border-b-2'>
             <h2 className='text-[24-px font-bold text-blue-400'>Twighlight Emporium Tracker</h2>
-            <h2 className='text-[24-px font-bold text-blue-500 hover:border-[2px] border-blue-500 rounded-md p-1 text-[15px] cursor-pointer'>LOGOUT</h2>
+            <button 
+                className='text-[24-px font-bold text-blue-500 hover:border-[2px] border-blue-500 rounded-md p-1 text-[15px] cursor-pointer'
+                onClick={() => setLoggedIn(() => !loggedIn)}    
+            >
+                    {!loggedIn ? (
+                        <Link to='/'>LOGOUT</Link>
+                    ) : (
+                        <Link to='/dashboard'>LOGIN</Link>
+                    )}
+            </button>
+        
+              
+            
+
         </div>
         
     </>
