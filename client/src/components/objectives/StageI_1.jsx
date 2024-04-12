43e0a8eb12c66683d7c44d0ccebc0bc6a5bdff69
Jordan Ferrario <108853449+JordanFerrario@users.noreply.github.com>
2024-04-11 21:39:39 -0700

    added functionality to the global timer and uploaded a placeholder image for the public objective

--- client/src/components/objectives/StageI_1.jsx
+++ client/src/components/objectives/StageI_1.jsx
@@ -1,6 +1,19 @@
+import React, { useState } from 'react';
+import objectiveFront from '../../assets/objective-front.png';
+import objectiveBack from '../../assets/objective-back.png';
+
 function StageI_1() {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  }
+
   return (
-    <div className="bg-gray-700 p-4 h-full">Stage I_1</div>
+    <div className="relative" onClick={handleFlip}>
+      <img src={objectiveFront}></img>
+      <img src={objectiveBack} className='hidden'></img>
+    </div>
   );
 }
 
