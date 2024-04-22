26d9dfb138d8234c82fb78186101882665e23bd0
gjudilla <gjudilla@gmail.com>
2024-04-21 17:45:17 -0700

    edited so gameclock and timer only start when host game button is clicked

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -6,25 +6,33 @@ import ActiveLaws from "./ActiveLaws";
 import PublicObjectives from "./PublicObjectives";
 import GameState from "./GameState";
 import StatusPhaseList from "./StatusPhaseList";
-import HostGameModal from "./HostGameModal"
+import HostGameModal from "./HostGameModal";
 
 function Dashboard({ phase }) {
     const isActionPhase = phase === "action";
 
     const [currentTurnTimer, setCurrentTurnTimer] = useState(0); // Timer for the current turn
+    const [isModalVisible, setModalVisible] = useState(true);
+    const [isGameHosted, setGameHosted] = useState(false); // Track whether the game has been hosted or not
 
     const handleNextTurn = () => {
         setCurrentTurnTimer(0); // Reset the timer when switching to the next turn
     };
 
-    const [isModalVisible, setModalVisible] = useState(true);
+
 
     const closeModal = () => {
-        setModalVisible(false)
-    }
+        setModalVisible(false);
+    };
+
+    const handleGameHosted = () => {
+        setGameHosted(true);
+        setModalVisible(false); // Close the modal after hosting the game
+    };
 
     return (
-        <div> {isModalVisible && <HostGameModal visible={isModalVisible} closeModal={closeModal} />}
+        <div>
+            {isModalVisible && <HostGameModal visible={isModalVisible} closeModal={closeModal} onGameHosted={handleGameHosted} />}
             <div className="grid grid-cols-2 grid-rows-3 gap-4">
                 <div className="col-start-1 row-start-1">
                     <Leaderboard />
@@ -33,33 +41,31 @@ function Dashboard({ phase }) {
                     <ActiveLaws />
                 </div>
                 <div className="col-start-2 row-start-1 row-span-2">
-                    <GameState>
-                        <div className="flex-1 mb-4">
-                            <GameClock />
-                        </div>
-                        {isActionPhase ? (
-                            /* Content for Action Phase GameState */
-                            <div className="col-span-1 row-span-2 flex flex-col gap-4">
-
-                                <div className="flex-1">
-                                    <Turn 
-                                        type="Current"
-                                        timer={currentTurnTimer}
-                                        setTimer={setCurrentTurnTimer}
-                                        />
-                                </div>
-                                <div className="flex-1">
-                                    <Turn
-                                        type="Next"
-                                        onEndTurn={handleNextTurn} 
-                                    />
-                                </div>
+                    {isGameHosted && (
+                        <GameState>
+                            <div className="flex-1 mb-4">
+                                <GameClock />
                             </div>
-                        ) : (
-                            /* Content for Status Phase GameState */
-                            <StatusPhaseList phase={phase} />
-                        )}
-                    </GameState>
+                            {isActionPhase ? (
+                                /* Content for Action Phase GameState */
+                                <div className="col-span-1 row-span-2 flex flex-col gap-4">
+                                    <div className="flex-1">
+                                        <Turn
+                                            type="Current"
+                                            timer={currentTurnTimer}
+                                            setTimer={setCurrentTurnTimer}
+                                        />
+                                    </div>
+                                    <div className="flex-1">
+                                        <Turn type="Next" onEndTurn={handleNextTurn} />
+                                    </div>
+                                </div>
+                            ) : (
+                                /* Content for Status Phase GameState */
+                                <StatusPhaseList phase={phase} />
+                            )}
+                        </GameState>
+                    )}
                 </div>
                 <div className="col-start-1 row-start-3 col-span-2">
                     <PublicObjectives />
@@ -67,7 +73,7 @@ function Dashboard({ phase }) {
 
             </div>
         </div>
-    )
+    );
 }
 
 export default Dashboard;
